8a797bfa1706d626b15e89275222402f2a46550e
mi11km <mi11km.basket@gmail.com>
2021-05-06 03:58:21 +0900

    implement chat page

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 
+import './Chat.css';
+
 interface Message {
     Name: string;
     Message: string;
@@ -15,6 +17,8 @@ const Chat = () => {
         e.preventDefault();
         if (!socket.current) {
             alert('エラー：Websocket接続なし');
+        } else if (!message) {
+            alert('メッセージを書いてください');
         } else {
             socket.current.send(JSON.stringify({ Message: message }));
             setMessage('');
@@ -26,35 +30,56 @@ const Chat = () => {
             alert('エラー：Websocketに対応していないブラウザです');
         } else {
             socket.current.onmessage = (e) => {
-                let msg: Message = JSON.parse(e.data);
+                const msg: Message = JSON.parse(e.data);
                 setMessageHistory([...messageHistory, msg]);
+                const chatScreen = document.getElementById('chat-screen');
+                if (chatScreen !== null) {
+                    const bottom = chatScreen.scrollHeight - chatScreen.clientHeight;
+                    chatScreen.scroll(0, bottom);
+                }
             };
         }
     }, [messageHistory]);
 
     return (
-        <>
-            <ul>
+        <div className="container m-auto h-screen w-full">
+            <h1 className="text-center my-6 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                WebSocketを使ったちゃっとあぷり
+            </h1>
+
+            <ul className="h-3/4 overflow-scroll" id="chat-screen">
                 {messageHistory.map((msg, index) => (
-                    <li key={index}>
-                        <strong>{msg.Name}: </strong>
-                        <span>{msg.Message}</span>
-                        <small> -- {msg.When}</small>
+                    <li key={index} className="my-1 flex justify-between">
+                        <div className="flex">
+                            <div>{msg.Name}:</div>
+                            <div className="arrow_box ml-12">{msg.Message}</div>
+                        </div>
+                        <div className="">
+                            <small> -- {msg.When}</small>
+                        </div>
                     </li>
                 ))}
             </ul>
-            WebSocketを使ったちゃっとあぷり
-            <form onSubmit={sendMessage}>
+
+            <form
+                onSubmit={sendMessage}
+                className="absolute bottom-0 my-4 lg:left-1/4 lg:right-1/4">
                 <textarea
                     name="chat"
-                    cols={60}
-                    rows={10}
+                    cols={100}
                     value={message}
                     onChange={(e) => setMessage(e.target.value)}
+                    className="focus:ring-indigo-500 focus:border-indigo-500 w-full px-3 sm:text-sm border-gray-300 rounded-md"
                 />
-                <input type="submit" value="送信" />
+                <div className="text-center">
+                    <input
+                        type="submit"
+                        value="送信"
+                        className="text-white bg-blue-500 w-4/5 h-12  mt-3 rounded-md hover:bg-blue-300 cursor-pointer"
+                    />
+                </div>
             </form>
-        </>
+        </div>
     );
 };
 
